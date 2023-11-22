e00ba834b2740cfb6d01d51dd2bd4ffd726c2122
Pranaw <71425637+itspranaw@users.noreply.github.com>
2023-11-22 13:57:39 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function changeText() {
+    var paragraph = document.querySelector('#main-content p');
+    paragraph.textContent = 'Congratulations! You clicked the button. This is OnClick function of JavaScript';
+}
+
+function sound(){
+    var snd = new Audio('k.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+
+function runAway() {
+    var button = document.getElementById('runawayButton');
+
+    // Get the window dimensions
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+
+    // Calculate a random position within the window
+    var randomX = Math.random() * (windowWidth - button.offsetWidth);
+    var randomY = Math.random() * (windowHeight - button.offsetHeight);
+
+    var snd = new Audio('huh-cat.mp3');
+    snd.play();
+
+    // Set the button's position
+    button.style.left = randomX + 'px';
+    button.style.top = randomY + 'px';
+}
